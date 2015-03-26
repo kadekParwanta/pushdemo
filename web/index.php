9e876468631a80c0a30b7f13e917a695924e504e
Kadek Parwanta <kadek.parwanta@access-company.com>
2015-03-26 23:33:30 +0800

    add /get_user route

--- web/index.php
+++ web/index.php
@@ -55,6 +55,21 @@ $app->get('/twig/{name}', function($name) use($app) {
 });
 
 
+$app->get('/get_user/', function() use($app) {
+  $st = $app['pdo']->prepare('select * FROM gcm_users');
+  $st->execute();
+
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['name']);
+    $names[] = $row;
+  }
+
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
+});
+
 $app->run();
 
 ?>
